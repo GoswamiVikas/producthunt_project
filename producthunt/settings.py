7643e7fbcd323dd437e49fd9bdb93b02efcd9d81
Vikas Goswami <vkas.mca17.du@gmail.com>
2018-09-20 16:22:51 +0530

    pipenv integration done

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'producthuntdb',
         'USER':'postgres',
-        'PASSWORD': 'postruth',
+        'PASSWORD': 'postgres',
         'HOST':'localhost',
         'PORT':'5432',
     }
